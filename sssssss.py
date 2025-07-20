eb611f58583b75f64b1ff37a6ea2bee706c7b720
Gunnoy1 <ohakjaol@gmail.com>
2025-07-21 00:32:04 +0700

    พีระมิด.py

--- sssssss.py
+++ sssssss.py
@@ -1,5 +1,3 @@
-n = int(input())
-w = len(" ".join(map(str, list(range(n,0,-1))+list(range(2,n+1)))))
-for i in range(1,n+1):
-    s = list(map(str, range(1,i+1)))
-    print(" ".join(s[1:][::-1]+s).center(w))
+n=int(input())
+w=len(" ".join([str(i)for i in list(range(n,0,-1))+list(range(2,n+1))]))
+for i in range(1,n+1):print(" ".join([str(j)for j in list(range(i,0,-1))+list(range(2,i+1))]).center(w))
