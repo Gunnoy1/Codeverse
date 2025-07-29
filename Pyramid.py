eb7ee10f534f3a1f14c0eb0adf958e4747ec2323
Gunnoy1 <ohakjaol@gmail.com>
2025-07-29 09:25:05 +0700

    Pyramid Number

--- Pyramid.py
+++ Pyramid.py
@@ -1,3 +1,3 @@
-n=int(input())
+n=int(input("Enter Number"))
 w=len(" ".join([str(i)for i in list(range(n,0,-1))+list(range(2,n+1))]))
 for i in range(1,n+1):print(" ".join([str(j)for j in list(range(i,0,-1))+list(range(2,i+1))]).center(w))
